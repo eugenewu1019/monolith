62993527aa13cdd9234d47f1228a5e9430090546
eugenewu1019 <eugenewu1019@users.noreply.github.com>
2026-07-04 14:01:02 +0000

    deploy: 69ca7e208142e7758b1a0082953beadf49091c05

---
+++ _next/static/lrzqf_xLtco-8I8JWSk0p/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Finsights\u002Fcollab\u002F[id]","\u002Fjournal\u002F[slug]","\u002Fmobile\u002Finsights\u002Fcollab\u002F[id]","\u002Fmobile\u002Fjournal\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
